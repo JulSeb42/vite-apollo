d53cb60167c63f3a00c230d26f55b203edd0399b
JulSeb42 <julien.sebag@me.com>
2023-05-04 21:40:27 +0200

    Clean

--- client/src/components/user/UserCard/UserCard.tsx
+++ client/src/components/user/UserCard/UserCard.tsx
@@ -2,14 +2,14 @@
 
 import { Text, Avatar } from "tsx-library-julseb"
 
-import * as Styles from "components/user/UserCard/styles"
+import { StyledUserCard } from "components/user/UserCard/styles"
 import type { UserCardProps } from "components/user/UserCard/types"
 
 export const UserCard = ({ user }: UserCardProps) => {
     return (
-        <Styles.StyledUserCard to={`/users/${user?._id}`} border={{ width: 1 }}>
+        <StyledUserCard to={`/users/${user?._id}`} border={{ width: 1 }}>
             <Avatar img={user?.avatar} size={32} />
             <Text tag="strong">{user?.fullName}</Text>
-        </Styles.StyledUserCard>
+        </StyledUserCard>
     )
 }
